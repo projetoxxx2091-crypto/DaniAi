ade62c79aae88a168690c45a1cbf27d0f2024a61
DaniAssistente <projetoxxx2091@gmail.com>
2025-09-07 22:48:33 -0300

    Add files via upload

---
+++ tmdb.js
@@ -0,0 +1,6 @@
+
+// tmdb.js - função de consulta catálogo + TMDB
+export const getTMDBDetails = async (title) => {
+    // função de consulta simulada
+    return { title: title, overview: "Sinopse do filme", release_date: "2021-01-01", type: "movie", seasons: 0 };
+}
